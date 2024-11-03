2315ecf24a347130063736996e0d894b78476216
mohuamouri <mohuaislammouri99@gmail.com>
2024-11-03 16:47:44 +0600

    change page size

--- students/views.py
+++ students/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets, pagination
 # Create your views here.
 
 class PageSizePagination(pagination.PageNumberPagination):
-    page_size = 5
+    page_size = 10
     page_size_query_param = "size"
 
 
