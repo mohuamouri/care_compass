f45eed63b83a8cf0ce9caf2d3c8a8ecfe993bdbb
Maria Mahtab Oyshi <oyshimaria@gmail.com>
2024-11-05 12:02:02 +0600

    laksuhjb

--- patient/admin.py
+++ patient/admin.py
@@ -9,9 +9,4 @@ class PatientAdmin(admin.ModelAdmin):
 admin.site.register(Patient, PatientAdmin)
 
 
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ('patient_name', 'age', 'gender', 'contact_number', 'emergency_contact', 'dr_name')
-    search_fields = ('patient_name', 'contact_number')
-    list_filter = ('gender', 'dr_name')
 
-admin.site.register(Patient, PatientAdmin)
